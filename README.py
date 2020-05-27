6a95471a803d5989c44479da9b6c71bfc380ac43
Benitez-Juarez-Jorge <jorgebenitezjuarez15@gmail.com>
2020-05-27 12:41:53 -0500

    ejercicio del 1 al 4
    benitez juarez jorge

---
+++ README.py
@@ -0,0 +1,4 @@
+print("1.- al hacer el programa se me hizo algo facil ya que solo tube que sumas dos numeros que heran sustituyidas por letras")
+print("2.- aqui tuve que investigar un poco, pero era igual un poco facil. ")
+print("3.-no comprendi muy bien, haci que tube que investigar y supe que los numeros feos son factores primos")
+print("4.-no comprendi muy bien, me ayudaron y haci pude realizar el programa, pero hasta donde se, es parecido al segundo problema")
